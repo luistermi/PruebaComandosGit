7026590f13e3fc99bc55e618cc3c2dcc7cab3b1a
luistermi <luis.termi.i@gmail.com>
2022-08-30 20:49:44 -0300

    seguimos con pruebas de tsc

--- typeScript/ejemplos3.js
+++ typeScript/ejemplos3.js
@@ -1,18 +1,7 @@
-"use strict";
-var a;
-var b;
-var c = 101;
-c = 2;
-var equipos = ["River", "Boca", 100];
-equipos = [1, 2, 3];
-function promedio(number1, number2) {
-    let divisor = 2;
-    return (number1 + number2) / 2;
-}
-let arraySinTipo = ["River", 2, true];
-let nombre = "Luis";
-let sobrenombre = "Luichi";
-let saludo = `Hola!!! mi nombre es ${nombre} y me dicen ${sobrenombre}.`;
+var arraySinTipo = ["River", 2, true];
+var nombre = "Luis";
+var sobrenombre = "Luichi";
+var saludo = "Hola!!! mi nombre es ".concat(nombre, " y me dicen ").concat(sobrenombre, ".");
 console.log(saludo);
 function saludar() {
     console.log("Hola cómo va!");
@@ -24,30 +13,30 @@ var Clubes;
     Clubes[Clubes["Flamengo"] = 2] = "Flamengo";
     Clubes[Clubes["Palmeiras"] = 3] = "Palmeiras";
 })(Clubes || (Clubes = {}));
-let s = 34;
-let hinchaDe = Clubes.River;
+var s = 34;
+var hinchaDe = Clubes.River;
 function mismoValor(valor) {
     return valor;
 }
 console.log(mismoValor(2));
 console.log(mismoValor(true));
 console.log(mismoValor([true, false, "River", 4]));
-let luis = { nombre: "Luis", edad: 38 };
+var luis = { nombre: "Luis", edad: 38 };
 console.log(luis.edad);
-let mari = { nombre: "Mari", edad: 30, fechaCumple: "31/08" };
+var mari = { nombre: "Mari", edad: 30, fechaCumple: "31/08" };
 // mari = {nombre: "Mari"};
 console.log(mari.edad);
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(numeros[2]);
-numeros.filter(numero => numero > 3);
-let nombres = ["Mari", "Clari", "Luli", "Rosi"];
-let [x, y, ...rest] = nombres;
+numeros.filter(function (numero) { return numero > 3; });
+var nombres = ["Mari", "Clari", "Luli", "Rosi"];
+var x = nombres[0], y = nombres[1], rest = nombres.slice(2);
 console.log(rest);
 console.log(x);
 console.log(y);
-let z = undefined;
-let w = null;
-let nombrecito;
+var z = undefined;
+var w = null;
+var nombrecito;
 nombrecito = "Un guacho Peola";
 console.log(nombrecito.charAt(0).toUpperCase());
 console.log(nombrecito.length);
--- typeScript/ejemplos3.ts
+++ typeScript/ejemplos3.ts
@@ -2,7 +2,7 @@ let arraySinTipo: any[] = ["River", 2, true];
 
 let nombre: string = "Luis";
 let sobrenombre: string = "Luichi";
-let saludo: string = `Hola!!! mi nombre es S{nombre} y me dicen S{sobrenombre}.`;
+let saludo: string = `Hola!!! mi nombre es ${nombre} y me dicen ${sobrenombre}.`;
 console.log(saludo);
 
 function saludar(): void {
@@ -16,6 +16,7 @@ enum Clubes {
     Palmeiras
 }
 
+let s: number = 34;
 let hinchaDe: Clubes = Clubes.River;
 function mismoValor<T>(valor: T): T {
     return valor;
@@ -28,5 +29,26 @@ console.log(mismoValor([true,false,"River",4]));
 let luis = {nombre: "Luis", edad: 38};
 console.log(luis.edad);
 
+let mari = {nombre: "Mari", edad: 30, fechaCumple: "31/08"};
+// mari = {nombre: "Mari"};
+console.log(mari.edad);
+
+let numeros: number[] = [1,2,3,4,5,6,7,8,9];
+console.log(numeros[2]);
+numeros.filter(numero => numero>3);
+
+let nombres: string[] = ["Mari", "Clari", "Luli", "Rosi"];
+let [x,y,...rest]: string[] = nombres;
+console.log(rest);
+console.log(x);
+console.log(y);
+
+let z: undefined = undefined;
+let w: null = null;
+
+let nombrecito;
+nombrecito = "Un guacho Peola";
+console.log((nombrecito as string).charAt(0).toUpperCase());
+console.log((<string>nombrecito).length);
 
 
